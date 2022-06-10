8133aa3cc7d0f0af1ab082eb3e8c9035b64d5557
AzzBotz22 <107181444+AzzBotz22@users.noreply.github.com>
2022-06-10 09:19:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,83 @@
+const fs = require("fs")
+const chalk = require("chalk")
+
+// Other
+global.thum = "https://telegra.ph/file/7188012d573871aaf4fa0.jpg"
+global.log0 = fs.readFileSync("./plugins/gambar/neobotz.jpg")
+global.thumb = fs.readFileSync("./plugins/gambar/neobotz.jpg")
+global.iklan = fs.readFileSync("./plugins/gambar/iklan.jpg")
+global.sewabot = fs.readFileSync("./plugins/gambar/sewa.jpg")
+global.qris = fs.readFileSync("./plugins/gambar/qris.jpg")
+global.err4r = "https://telegra.ph/file/7188012d573871aaf4fa0.jpg"
+global.owner = ['62859194145686','62859194145686']
+global.ownernumber = "62859194145686"
+global.ownername = "ᴿ᭄ AZZ ×፝֟͜×"
+global.namebot = "©AzzBotz"
+global.lolhuman = "RelzzAPIs"
+global.rkyt = []
+global.banUser = ['6281366679021@s.whatsapp.net']
+global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
+global.wlcm = []
+global.gcrevoke = ['19054091487-1424650263@g.us']
+global.packname = "© AzzBotz"
+global.packname2 = "ᴿ᭄ AZZ ×፝֟͜×"
+global.author = "ᴿ᭄ AZZ ×፝֟͜×"
+global.sessionName = "session"
+global.linkgrupss = "https://chat.whatsapp.com/G3ydGzHeZ825IU4RjLuToY"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: '*_Done Ngab 🤗_*',
+    admin: '*_Lu Admin? 🤨_*',
+    botAdmin: '*_Bot Bukan Admin 😔_*',
+    owner: '*_Lu Bukan Owner Gw 😠_*',
+    group: '*_Khusus Group 😏_*',
+    private: '*_Khusus Private Chat_*',
+    bot: '*_Khusus Pengguna Nomer Bot_*',
+    wait: '*_Sabar Sedang Di Proses ..._*',
+    linkm: 'Linknya Mana Kak?',
+    ban: '*Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner*'
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 999,
+    monayawal: 99999
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 999
+}
+// Beli Di Zenz Yo
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Isi Api Mu Di sini
+global.APIKeys = {
+	'https://zenzapis.xyz': 'BagasPrdn',
+	'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://violetics.pw': 'beta',
+}
+//Thank Fax Bot
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
